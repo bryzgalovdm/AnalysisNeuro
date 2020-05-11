1995bfd238fd654faf9d380c1bd2d2bc99ce4d20
bryzgalovdm <bryzgalovdm@gmail.com>
2020-05-11 10:58:38 +0200

    added mouse-by mouse number

--- Spikes/PlaceCells/CountPlaceCells.m
+++ Spikes/PlaceCells/CountPlaceCells.m
@@ -1,14 +1,34 @@
-function numPCs = CountPlaceCells(Dir)
+function [numPCs, numperM] = CountPlaceCells(Dir, varargin)
 
 % CountPlaceCells
 % 
-% Script that quickly counts all recorded place cells
+% Function that quickly counts all recorded place cells
 % gets them from SpikeData.PlaceCells field
 % 
 % See also FindAllPlaceFields
 
+%% Optional arguments
+for i=1:2:length(varargin)
+    
+    switch(lower(varargin{i}))
+        
+        case 'verbose'
+            verbose = varargin{i+1};
+            if ~isa(verbose,'logical')
+                error('Incorrect value for property ''Verbose'' (type ''help PlaceCellCrossValidation'' for details).');
+            end
+    end
+end
+
+try
+    verbose;
+catch
+    verbose = true;
+end
+
 %% Count
 numPCs = 0;
+numperM = zeros(1, length(Dir.path));
 for j=1:length(Dir.path)
     
     % Get the data
@@ -16,12 +36,15 @@ for j=1:length(Dir.path)
     load('SpikeData.mat','PlaceCells');
     if isfield(PlaceCells,'idx')
         numPCs = numPCs + length(PlaceCells.idx);
+        numperM(j) = length(PlaceCells.idx);
         clear PlaceCells
     end
 end
 
-disp('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-fprintf('%2i place cells in the analysis\n', numPCs);
-disp('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+if verbose
+    disp('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+    fprintf('%2i place cells in the analysis\n', numPCs);
+    disp('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+end
 
 end
